68561a85590b73f90731117318f7128866f09426
SirCollypso <aminkapkan444@gmail.com>
2023-05-29 02:42:03 +0900

    test(smart-contract): add full-cover tests

--- smart_contract/test/EcoToken.js
+++ smart_contract/test/EcoToken.js
@@ -4,8 +4,8 @@ const { expect } = require("chai");
 describe("EcoToken", function () {
   
   async function deployEcoTokenFixture() {
-    const baseValueEth = 1;
-    const baseValueEcoToken = 1;
+    const baseValueEth = 3;
+    const baseValueEcoToken = 100;
     const rewardRate = 1;
 
     const EcoToken = await ethers.getContractFactory("EcoToken");
@@ -19,138 +19,210 @@ describe("EcoToken", function () {
       addr3: (await ethers.getSigner(account3Address)) };
   }
 
-  describe("Deployment", function () {
+  describe("deployment", function () {
     
-    let _ecoToken;
-    let _addr1, _addr2, _addr3;
-
-    before("Deploy the contract instance first", async function () {
+    it("Should correctly set default balance", async function () {
       const {ecoToken, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
-      _ecoToken = ecoToken;
-      _addr1 = addr1;
-      _addr2 = addr2;
-      _addr3 = addr3;
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(0);
+      expect(await ecoToken.connect(addr2).getClientBalance()).to.equal(0);
+      expect(await ecoToken.connect(addr3).getClientBalance()).to.equal(0);
     });
 
-    it("Should set correct default balance", async function () {
-      expect(await _ecoToken.connect(_addr1).getClientBalance()).to.equal(0);
-      expect(await _ecoToken.connect(_addr2).getClientBalance()).to.equal(0);
-      expect(await _ecoToken.connect(_addr3).getClientBalance()).to.equal(0);
+    it("Should correctly set default business status", async function () {
+      const {ecoToken, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      expect(await ecoToken.connect(addr1).getBusinessStatus()).to.equal(false);
+      expect(await ecoToken.connect(addr2).getBusinessStatus()).to.equal(false);
+      expect(await ecoToken.connect(addr3).getBusinessStatus()).to.equal(false);
     });
 
-    it("Should set correct default business status", async function () {
-      expect(await _ecoToken.connect(_addr1).getBusinessStatus()).to.equal(false);
-      expect(await _ecoToken.connect(_addr2).getBusinessStatus()).to.equal(false);
-      expect(await _ecoToken.connect(_addr3).getBusinessStatus()).to.equal(false);
-    });
-    /*
-    it("Should set the correct owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-    */
   });
 
-  describe("Business Registration", function () {
+  describe("registerAsBusiness", function () {
     
-    it("Should properly register business accounts", async function () {
+    it("Should correctly register business accounts", async function () {
       const {ecoToken, baseValueEth, addr1, addr2} = await loadFixture(deployEcoTokenFixture);
       await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
       expect(await ecoToken.connect(addr1).getBusinessStatus()).to.equal(true);
-      await expect(ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth})).to.be.revertedWith("Only non-business clients can call this function");
-      await expect(ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth - 1 })).to.be.revertedWith("Insufficient Ether sent for registration");
       expect(await ecoToken.connect(addr2).getBusinessStatus()).to.equal(false);
       await ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth + 1 });
       expect(await ecoToken.connect(addr2).getBusinessStatus()).to.equal(true);
     });
 
+    it("Should correctly setup business balances", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, addr1, addr2} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken);
+      expect(await ecoToken.connect(addr2).getClientBalance()).to.equal(0);
+      await ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth + 1 });
+      expect(await ecoToken.connect(addr2).getClientBalance()).to.equal(baseValueEcoToken);
+    });
+
+    it("Should correctly hadnle incorrect requests", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, addr1, addr2} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken);
+      await expect(ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth - 1 })).to.be.revertedWith("Insufficient Ether sent for registration");
+      await expect(ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth})).to.be.revertedWith("Only non-business clients can call this function");
+    });
+
   });
 
-  /*
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+  describe("createEvent", function() {
+    
+    it("Should correctly create events", async function() {
+      const { ecoToken, addr1, addr2, addr3, baseValueEth } = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      expect(await ecoToken.connect(addr1).getCreatedEventCount()).to.equal(1);
+      expect(await ecoToken.connect(addr2).getCreatedEventCount()).to.equal(0);
+      expect(await ecoToken.connect(addr1).getEventCount()).to.equal(1);      
+      await ecoToken.connect(addr2).createEvent("Event2", 1);
+      expect(await ecoToken.connect(addr2).getCreatedEventCount()).to.equal(1);
+      expect(await ecoToken.connect(addr1).getCreatedEventCount()).to.equal(1);
+      expect(await ecoToken.connect(addr1).getEventCount()).to.equal(2);
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+    it("Should correcly handle incorrect events", async function () {
+      const { ecoToken, addr1, addr2, addr3, baseValueEth } = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      await ecoToken.connect(addr2).createEvent("Event2", 1);
+      await expect(ecoToken.connect(addr3).createEvent("Event3", 1)).to.be.revertedWith("Only business clients can call this function");
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
   });
-  */
+
+  describe("markParticipant", function() {
+
+    it("Should correctly mark participants", async function() {
+      const {ecoToken, baseValueEth, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      await ecoToken.connect(addr1).createEvent("Event2", 1);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 1);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 1);
+      expect(await ecoToken.connect(addr2).getParticipatedEventCount()).to.equal(2);
+      expect(await ecoToken.connect(addr3).getParticipatedEventCount()).to.equal(1);
+      expect((await ecoToken.connect(addr2).getParticipatedEvent(0)).description).to.equal("Event1");
+      expect((await ecoToken.connect(addr2).getParticipatedEvent(1)).description).to.equal("Event2");
+      expect((await ecoToken.connect(addr3).getParticipatedEvent(0)).description).to.equal("Event2");
+    });
+
+    it("Should correctly distribute rewards", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      await ecoToken.connect(addr1).createEvent("Event2", 2);
+      await ecoToken.connect(addr1).createEvent("Event3", 3);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 2);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 1);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 2);
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken - 1 - 3 - 2 - 3);
+      expect(await ecoToken.connect(addr2).getClientBalance()).to.equal(1 + 3);
+      expect(await ecoToken.connect(addr3).getClientBalance()).to.equal(2 + 3);
+    });
+
+    it("Should correctly handle incorrect requests", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr2).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", baseValueEcoToken + 1);
+      await ecoToken.connect(addr1).createEvent("Event2", baseValueEcoToken);
+      await expect(ecoToken.connect(addr2).markParticipant(addr3.address, 0)).to.be.revertedWith("Only the event organizer can mark participants");
+      await expect(ecoToken.connect(addr1).markParticipant(addr2.address, 1)).to.be.revertedWith("Only non-business clients can be marked as participants");
+      await expect(ecoToken.connect(addr1).markParticipant(addr3.address, 0)).to.be.revertedWith("Insufficient EcoTokens");
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 1);
+      await expect(ecoToken.connect(addr1).markParticipant(addr3.address, 1)).to.be.revertedWith("Insufficient EcoTokens");
+    });
+
+  });
+
+  describe("voteForEvent", async function() {
+    
+    it("Should correctly change ratings", async function() {
+      const {ecoToken, baseValueEth, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 0);
+      await ecoToken.connect(addr2).voteForEvent(0, -1);
+      expect(await ecoToken.connect(addr2).getVoteStatus(0)).to.equal(-1);
+      await ecoToken.connect(addr3).voteForEvent(0, -1);
+      expect(await ecoToken.connect(addr3).getVoteStatus(0)).to.equal(-1);
+      expect ((await ecoToken.connect(addr2).getParticipatedEvent(0)).rating).to.equal(-2);
+      expect ((await ecoToken.connect(addr3).getParticipatedEvent(0)).rating).to.equal(-2);
+      expect ((await ecoToken.connect(addr2).getEvent(0)).rating).to.equal(-2);
+      expect ((await ecoToken.connect(addr3).getEvent(0)).rating).to.equal(-2);
+    });
+
+    it("Should correctly handle incorrect requests", async function() {
+      const {ecoToken, baseValueEth, addr1, addr2} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 1);
+      await ecoToken.connect(addr1).createEvent("Event2", 1);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await expect(ecoToken.connect(addr2).voteForEvent(0, 0)).to.be.revertedWith("Invalid vote value");
+      expect(await ecoToken.connect(addr2).getVoteStatus(0)).to.equal(0);
+      await expect(ecoToken.connect(addr2).voteForEvent(1, -1)).to.be.revertedWith("You must participate in the event before voting");
+      await expect(ecoToken.connect(addr2).getVoteStatus(1)).to.be.revertedWith("You have not participated in the event");
+      await ecoToken.connect(addr2).voteForEvent(0, -1);
+      expect(await ecoToken.connect(addr2).getVoteStatus(0)).to.equal(-1);
+      await expect(ecoToken.connect(addr2).voteForEvent(0, -1)).to.be.revertedWith("You can only vote once for an event");
+      expect(await ecoToken.connect(addr2).getVoteStatus(0)).to.equal(-1);
+    });
+
+  });
+
+  describe("transferEcotokens", async function() {
+    
+    it("Should correctly transfer without reward", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, rewardRate, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 10);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 0);
+      await ecoToken.connect(addr2).transferEcotokens(addr1.address, 4);
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken - 10 - 10 + 4);
+      await ecoToken.connect(addr3).transferEcotokens(addr1.address, 5);
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken - 10 - 10 + 4 + 5);
+    });
+
+    it("Should correctly transfer with reward", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, rewardRate, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 10);
+      await ecoToken.connect(addr1).createEvent("Event2", 10);
+      await ecoToken.connect(addr1).createEvent("Event3", 10);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 1);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 2);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 1);
+      await ecoToken.connect(addr2).voteForEvent(0, 1);
+      await ecoToken.connect(addr2).voteForEvent(1, 1);
+      await ecoToken.connect(addr2).transferEcotokens(addr1.address, 1);
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken - 10 - 10 - 10 - 10 - 10 + 1 + 2 / rewardRate);
+      await ecoToken.connect(addr2).voteForEvent(2, -1);
+      await ecoToken.connect(addr3).voteForEvent(0, -1);
+      await ecoToken.connect(addr3).voteForEvent(1, -1);
+      await ecoToken.connect(addr3).transferEcotokens(addr1.address, 1);
+      expect(await ecoToken.connect(addr1).getClientBalance()).to.equal(baseValueEcoToken - 10 - 10 - 10 - 10 - 10 + 1 + 2 / rewardRate + 1);
+    });
+
+    it("Should correctly handle incorrect requests", async function() {
+      const {ecoToken, baseValueEth, baseValueEcoToken, rewardRate, addr1, addr2, addr3} = await loadFixture(deployEcoTokenFixture);
+      await ecoToken.connect(addr1).registerAsBusiness({ value: baseValueEth });
+      await ecoToken.connect(addr1).createEvent("Event1", 10);
+      await ecoToken.connect(addr1).markParticipant(addr2.address, 0);
+      await ecoToken.connect(addr1).markParticipant(addr3.address, 0);
+      await expect(ecoToken.connect(addr2).transferEcotokens(addr3.address, 1)).to.be.revertedWith("You can only transfer EcoTokens to a business client");
+      await expect(ecoToken.connect(addr2).transferEcotokens(addr1.address, 11)).to.be.revertedWith("Insufficient EcoTokens");
+    });
+
+  });
+
 });
