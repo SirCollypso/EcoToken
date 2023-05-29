1aabcf99f7bffac5c7ba5d31c8ca59183331da2a
Nurlykhan Kopenov <nurlykhan.kopenov@gmail.com>
2023-05-30 04:10:10 +0900

    aboutcontent added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Navbar, Company, Footer } from "../components";
+import AboutContent from "../components/AboutContent";
+import animated from "../../images/animated.svg";
+import ethereumLogo from "../../images/ethereumLogo.svg";
 
 const About = () => (
-  <div className="flex flex-col min-h-screen">
-    <div className="flex-1 gradient-bg-welcome">
+  <div className="flex flex-col">
+    <div className="gradient-bg-welcome">
       <Navbar />
-      <Company />
+    </div>
+    <div className="flex flex-1 flex-col gradient-bg-content items-center">
+      <Company/>
+      <AboutContent positionId={1} logo={animated}/>
+      <AboutContent positionId={2} logo={ethereumLogo}/>
     </div>
     <Footer />
   </div>
