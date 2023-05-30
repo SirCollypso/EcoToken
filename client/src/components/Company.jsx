63d7e8122c5234bbbccf08dc5a65da7fe7a93a36
Nurlykhan Kopenov <nurlykhan.kopenov@gmail.com>
2023-05-30 19:18:29 +0900

    aboutcontent change height

--- client/src/components/Company.jsx
+++ client/src/components/Company.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Company = () => {
   return (
-    <div className="flex h-[60vh] w-full justify-center items-center">
+    <div className="flex h-[60vh] w-full justify-center items-center m-auto header-font">
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-center items-center flex-col mf:mr-10">
-          <p className="text-center mt-5 text-white font-light md:w-9/12 w-11/12 text-6xl">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+          <p className="text-center mt-5 text-white font-light md:w-9/12 w-11/12 text-3xl">
+          In today's world, environmental problems have become more pressing than ever before, and we humanity is in a dire need of innovative solutions to encourage individuals to adopt sustainable behaviour.
           </p>
         </div>
       </div>
